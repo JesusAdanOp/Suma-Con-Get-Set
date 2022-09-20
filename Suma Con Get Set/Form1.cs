2226ecbde56a3645d8a9e4f648d2074dc41e1c51
JesusAdanOp <jesusadanorozcop@outlook.com>
2022-09-20 08:02:57 -0600

    Agregar archivos de proyecto.

---
+++ Suma Con Get Set/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Suma_Con_Get_Set
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            clsuma suma = new clsuma();
+            suma.V1 = int.Parse(txtv1.Text);
+            suma.V2 = int.Parse(txtv2.Text);
+
+            txtresult.Text = suma.sumar().ToString();
+        }
+    }
+}
---
+++ Suma Con Get Set/clsuma.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma_Con_Get_Set
+{
+    internal class clsuma
+    {
+        int v1, v2;
+
+        public int V1
+        {
+            get { return v1; }
+            set { v1 = value; }
+        }
+
+        public int V2
+        {
+            get { return v2; }
+            set { v2 = value; }
+        }
+
+        public int sumar()
+        {
+            return v1+ v2;
+        }
+    }
+}
